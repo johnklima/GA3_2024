e325268e0c2f34b92c8cdaebcdd0fc43fb3a8bda
johnklima <klima.de.sa@gmail.com>
2024-04-07 13:05:01 +0200

    begin final mary

--- Assets/Mary Lamb/scripts/NarrativeRoot.cs
+++ Assets/Mary Lamb/scripts/NarrativeRoot.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/* This module handles dialog tree behaviour. To enable re-entrant dialog, the DialogElement
+ * has an additional reference, which one could imagine as just another child empty in the
+ * scene graph. As a scene graphic is "acyclic" we need to provide an "out point" to somewhere else
+ * in the scene graph, thus making a DAG into DCG 
+ * (Directed Acyclic Graph, into a Directed Cyclic Graph."
+*/
+
 public class NarrativeRoot : MonoBehaviour
 {
     public Transform currentDecision;  //our current position in the tree    
@@ -15,16 +22,23 @@ public class NarrativeRoot : MonoBehaviour
         {
             t.gameObject.SetActive(false);
         }
+
+
         //chose the branch assuming it exists
         if (currentDecision.childCount > index)
         {
             currentDecision.GetChild(index).gameObject.SetActive(true);
             currentDecision = currentDecision.GetChild(index);
+            DialogElement de = currentDecision.GetComponent<DialogElement>();
+            de.RefreshText();
         }
-        else //if no children, check if the component has a "next" node re-entry point
+        else
+        //if no children, check if the component has a "next" node re-entry point
+        //which is the equavalent to having one more child
         {
             DialogElement de = currentDecision.GetComponent<DialogElement>();
 
+            //does this have a "re-entrant" child node?
             if (de.nextNode)
             {
                 de.nextNode.GetComponent<DialogElement>().RefreshText();

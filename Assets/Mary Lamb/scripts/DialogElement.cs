5cadf3f45ce8f3557f95e7f4626d8585d15f395d
johnklima <klima.de.sa@gmail.com>
2024-04-10 00:47:11 +0200

    dialog system damn near perfect

--- Assets/Mary Lamb/scripts/DialogElement.cs
+++ Assets/Mary Lamb/scripts/DialogElement.cs
@@ -6,23 +6,27 @@ using UnityEngine.UI;
 public class DialogElement : MonoBehaviour
 {
 
-    public string elementText;
+    [TextArea(10,10)]public string elementText;
     public Text textElement;
     public Transform nextNode;
 
+    public Text buttonText;
 
-    private void OnEnable()
-    {
-        RefreshText();
-    }
-    private void OnDisable()
-    {
-        
-    }
+    
 
     public void RefreshText()
     {
-        textElement.text = elementText;
+        //keep telling the story!
+        textElement.text = textElement.text + " " + elementText;
+        
+    }
+    public void RefreshButton()
+    {
+        if(buttonText != null)
+        {
+            buttonText.text = elementText;
+        }
+        
     }
 }
 
--- Assets/Mary Lamb/scripts/NarrativeRoot.cs
+++ Assets/Mary Lamb/scripts/NarrativeRoot.cs
@@ -14,40 +14,72 @@ public class NarrativeRoot : MonoBehaviour
 {
     public Transform currentDecision;  //our current position in the tree    
 
+    private void Start()
+    {
+        DialogElement de = currentDecision.GetComponent<DialogElement>();
+        de.RefreshText();
+
+        foreach (Transform t in currentDecision)
+        {
+            t.GetComponent<DialogElement>().RefreshButton();
+        }
+
+
+
+    }
+
     public void Choice(int index)   //index maps to the button that was pressed
     {                               //or some other mechanism that made a choice
         
-        //set all children to false
-        foreach (Transform t in currentDecision)
-        {
-            t.gameObject.SetActive(false);
-        }
-
-
         //chose the branch assuming it exists
         if (currentDecision.childCount > index)
         {
-            currentDecision.GetChild(index).gameObject.SetActive(true);
+            //increment the decision 
             currentDecision = currentDecision.GetChild(index);
             DialogElement de = currentDecision.GetComponent<DialogElement>();
-            de.RefreshText();
+
+            de.RefreshText(); //populate text box with its current level
+
+            //does this one now have a reentry node?
+            if(de.nextNode)
+            {
+                //refresh each button with the next decision options
+                foreach (Transform t in de.nextNode)
+                {
+                    t.GetComponent<DialogElement>().RefreshButton();
+                }
+                //and it gets even more crazy, show the answer as a question
+                //de.nextNode.GetComponent<DialogElement>().RefreshText();
+            }
+            //WOW!! in the case of no kids, but a nextNode, make the nextNode current
+            //as each node can be both a question and an answer, or maybe neither
+            if (currentDecision.childCount == 0 && de.nextNode)
+            {
+                currentDecision = de.nextNode;
+                //and it gets even more crazy
+                currentDecision.GetComponent<DialogElement>().RefreshText();
+            }
+
         }
-        else
-        //if no children, check if the component has a "next" node re-entry point
-        //which is the equavalent to having one more child
+        else if (currentDecision.childCount == 0)
         {
+
+            //if no children, check if the component has a "next" node re-entry point
+            //which is the equavalent to having one more child
+            
             DialogElement de = currentDecision.GetComponent<DialogElement>();
 
             //does this have a "re-entrant" child node?
             if (de.nextNode)
             {
+                //show the text from the NEXT next node, this next node??
                 de.nextNode.GetComponent<DialogElement>().RefreshText();
                 currentDecision = de.nextNode;
-                currentDecision.gameObject.SetActive(true);
-
-            }
-
-        }
-        
+                foreach (Transform t in de.nextNode)
+                {
+                    t.GetComponent<DialogElement>().RefreshButton();
+                }
+            }           
+        }        
     }
 }
